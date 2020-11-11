53dccd1b740727bdcb82cb95b8cc688cff12b148
ozgeguner <ozgecaylioglu@gmail.com>
2020-11-11 23:20:54 +0000

    added authentication

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-
+  # http_basic_authenticate_with name: "Jungle", password: "book"
   def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
