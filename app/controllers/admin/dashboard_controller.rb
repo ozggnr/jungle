e3ede81f591a9014094d94c66a3557671475295e
ozgeguner <ozgecaylioglu@gmail.com>
2020-11-12 00:54:19 +0000

    added general stats about store

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
   def show
-  end
+    @count_products = Product.count
+    @category_count = Product.count('DISTINCT category_id')
+  end 
 end
