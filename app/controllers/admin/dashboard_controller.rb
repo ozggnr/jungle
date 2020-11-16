0b4ab46df2a30990341394e4a8dec1328a7c1357
ozgeguner <ozgecaylioglu@gmail.com>
2020-11-16 18:50:11 +0000

    applied some test

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: "Jungle", password: "book"
   def show
     @count_products = Product.count
     @category_count = Product.count('DISTINCT category_id')
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
+      flash.now.alert = "Email or password is invalid"
       redirect_to '/login'
     end
   end
